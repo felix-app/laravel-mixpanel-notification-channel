972ec2159646f56da0b5b4635f459fdfdf010d84
josemf <josemataf@gmail.com>
2017-03-01 19:28:45 +0000

    A working Mixpanel notifications channel provider. Supports user set, identify and track events

--- src/MixpanelChannel.php
+++ src/MixpanelChannel.php
@@ -4,9 +4,17 @@ namespace NotificationChannels\Mixpanel;
 
 use Illuminate\Notifications\Notification;
 
+use Mixpanel;
+
 class MixpanelChannel
 {
-
+    protected $mixpanel;
+    
+    public function __construct(Mixpanel $mixpanel)
+    {
+        $this->mixpanel = $mixpanel;
+    }
+    
     /**
      * Send the given notification.
      *
@@ -16,6 +24,20 @@ class MixpanelChannel
      */
     public function send($notifiable, Notification $notification)
     {
+        $message = $notification->toMixpanel($notifiable);
+        
+        if(!empty($message->identity)) {
+            $this->mixpanel->identify($message->identity);
+            
+            if(!empty($message->person)) {
+                $this->mixpanel->people->set($message->identity, $message->person);
+            }            
+        }
 
+        if(!empty($message->trackEvents)) {
+            foreach($message->trackEvents as $event) {
+                $this->mixpanel->track($event['name'], $event['parameters']);
+            }
+        }
     }
 }
--- src/MixpanelMessage.php
+++ src/MixpanelMessage.php
@@ -2,7 +2,53 @@
 
 namespace NotificationChannels\Mixpanel;
 
+use NotificationChannels\Mixpanel\Contracts\MixpanelIdentifiable;
+
 class MixpanelMessage
 {
+    public $identity;
+    public $trackEvents = [];
+    public $person;
 
+    public function identity($identity) {
+        $this->identity = $identity;
+
+        return $this;
+    }
+    
+    public function track($event, array $parameters = []) {
+        $this->trackEvents []= [
+            'name' => $event,
+            'parameters' => $parameters
+        ];
+
+        return $this;
+    }
+
+    public function person($firstName, $lastName = null, $email = null, $phone = null, $arguments = null)
+    {
+        $person = [
+            '$first_name' => $firstName
+        ];
+
+        if(!empty($lastName)) {
+            $person['$last_name'] = $lastName;
+        }
+
+        if(!empty($email)) {
+            $person['$email'] = $email;
+        }
+
+        if(!empty($phone)) {
+            $person['$phone'] = $phone;
+        }
+
+        if(!empty($arguments)) {
+            $person = array_merge($person, $arguments);
+        }
+
+        $this->person = $person;
+
+        return $this;
+    }
 }
