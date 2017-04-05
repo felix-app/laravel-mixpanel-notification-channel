d12d3296df4e4defceba7f11cd310c7c50121d30
josemf <josemataf@gmail.com>
2017-04-05 19:56:58 +0100

    More complete Mixpanel API support

---
+++ src/MixpanelChannelException.php
@@ -0,0 +1,7 @@
+<?php namespace NotificationChannels\Mixpanel;
+
+use Exception;
+
+class MixpanelChannelException extends Exception
+{
+}
--- src/MixpanelMessage.php
+++ src/MixpanelMessage.php
@@ -4,11 +4,18 @@ namespace NotificationChannels\Mixpanel;
 
 use NotificationChannels\Mixpanel\Contracts\MixpanelIdentifiable;
 
+use Carbon\Carbon;
+
 class MixpanelMessage
 {
     public $identity;
     public $trackEvents = [];
     public $person;
+    public $properties = [];
+    public $alias;
+    public $increments = [];
+    public $appends = [];
+    public $charges = [];
 
     public function identity($identity) {
         $this->identity = $identity;
@@ -50,5 +57,49 @@ class MixpanelMessage
         $this->person = $person;
 
         return $this;
+    }
+
+    public function alias($identity)
+    {
+        $this->alias = $identity;        
+        return $this;
+    }
+
+    public function increment($property, $count = 1)
+    {
+        if(empty($this->increments[$property])) {
+            $this->increments[$property] = 0;
+        }
+        
+        $this->increments[$property] += (int)$count;
+
+        return $this;
+    }
+
+    public function append($property, $value)
+    {
+        if(empty($this->appends[$property])) {
+            $this->appends[$property] = [];
+        }
+
+        $this->appends[$property][] = $value;
+
+        return $this;
+    }
+
+    public function charge($amount, Carbon $date = null)
+    {
+        if(empty($data)) {
+            $this->charges []= $amount; 
+        } else {
+            $this->charges []= [ $amount, $date ];
+        }        
+
+        return $this;
+    }
+
+    public function property($property, $value)
+    {
+        $this->properties[$property] = $value;
     }
 }
